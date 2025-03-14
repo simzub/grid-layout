61c833dd843ab056dea8ee33664f441165dec19d
simzub <78310955+simzub@users.noreply.github.com>
2025-03-14 22:35:08 +0200

    Add JS and separate css file

---
+++ index.js
@@ -0,0 +1,12 @@
+const toggleButton = document.querySelector('.expand');
+const sidebar = document.querySelector('.sidebar');
+
+toggleButton.addEventListener('click', function () {
+    sidebar.classList.toggle('collapsed');
+
+    if (sidebar.classList.contains('collapsed')) {
+        toggleButton.textContent = '>';
+    } else {
+        toggleButton.textContent = 'X';
+    }
+})
